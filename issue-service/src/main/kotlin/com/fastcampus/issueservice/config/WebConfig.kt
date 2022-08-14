dd56dfed124dec21f22dc80c088d58648376cfde
jn.park <jn.park@navercorp.com>
2022-08-14 12:50:37 +0900

    작업중

--- issue-service/src/main/kotlin/com/fastcampus/issueservice/config/WebConfig.kt
+++ issue-service/src/main/kotlin/com/fastcampus/issueservice/config/WebConfig.kt
@@ -5,29 +5,27 @@ import org.springframework.core.MethodParameter
 import org.springframework.stereotype.Component
 import org.springframework.web.bind.support.WebDataBinderFactory
 import org.springframework.web.context.request.NativeWebRequest
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
 import org.springframework.web.method.support.ModelAndViewContainer
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport
 
 @Configuration
 class WebConfig(
     private val authUserHandlerArgumentResolver: AuthUserHandlerArgumentResolver,
-) : WebMvcConfigurationSupport() {
-
-
+): WebMvcConfigurationSupport() {
     override fun addArgumentResolvers(argumentResolvers: MutableList<HandlerMethodArgumentResolver>) {
         argumentResolvers.apply {
             add(authUserHandlerArgumentResolver)
         }
     }
-
 }
 
 @Component
-class AuthUserHandlerArgumentResolver : HandlerMethodArgumentResolver {
+class AuthUserHandlerArgumentResolver: HandlerMethodArgumentResolver {
 
     override fun supportsParameter(parameter: MethodParameter): Boolean =
         AuthUser::class.java.isAssignableFrom(parameter.parameterType)
+
 
     override fun resolveArgument(
         parameter: MethodParameter,
@@ -35,13 +33,11 @@ class AuthUserHandlerArgumentResolver : HandlerMethodArgumentResolver {
         webRequest: NativeWebRequest,
         binderFactory: WebDataBinderFactory?
     ): Any? {
-
         return AuthUser(
             userId = 1,
             username = "테스트",
         )
     }
-
 
 }
 
@@ -50,4 +46,3 @@ data class AuthUser(
     val username: String,
     val profileUrl: String? = null,
 )
-
