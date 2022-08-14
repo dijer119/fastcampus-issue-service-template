a3c368dfe59a0dfbc15139cbc2dfd0d25a194378
jn.park <jn.park@navercorp.com>
2022-08-15 07:53:41 +0900

    작업중

---
+++ issue-service/src/main/kotlin/com/fastcampus/issueservice/domain/IssueRepository.kt
@@ -0,0 +1,5 @@
+package com.fastcampus.issueservice.domain
+
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface IssueRepository: JpaRepository<Issue, Long>
---
+++ issue-service/src/main/kotlin/com/fastcampus/issueservice/domain/enums/IssueType.kt
@@ -0,0 +1,14 @@
+package com.fastcampus.issueservice.domain.enums
+
+enum class IssueType {
+    BUG, TASK;
+
+    companion object {
+        operator fun  invoke(type: String) = valueOf(type.uppercase())
+    }
+}
+
+fun main() {
+    val type = IssueType("BUG")
+    println(IssueType.BUG == type)
+}
